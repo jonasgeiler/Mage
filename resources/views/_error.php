82c158e4b629e8a23ba7cd178775931ee4fe4447
Skayo <jonas@skayo.dev>
2021-04-07 00:34:14 +0200

    Added more head tags to _error.php

--- resources/views/_error.php
+++ resources/views/_error.php
@@ -3,8 +3,16 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="theme-color" content="#0096bf" />
 
-		<title><?= $code ?> - <?= $status ?></title>
+		<title><?= $status ?> - Mage, by Skayo</title>
+
+		<link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
+		<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
+		<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
+		<link rel="manifest" href="/manifest.json" />
+
+		<script async defer data-domain="mage.skayo.dev" src="https://analytics.skayo.dev/js/plausible.js"></script>
 
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" />
 		<style type="text/css">
